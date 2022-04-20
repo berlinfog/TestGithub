57f0834d04c325eb421c4eaeac53cb4b5d439ef8
berlinfog <k1075060506@gmail.com>
2022-04-20 13:53:42 -0700

    add new file string.py

---
+++ sample/src/string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+var1 = 'Hello World!'
+var2 = "Python Programming"
+
+print ("var1[0]: ", var1[0])
+print ("var2[1:5]: ", var2[1:5]) # 切片加索引//原文出自【易百教程】，商业转载请联系作者获得授权，非商业请保留原文链接：https://www.yiibai.com/git/git_fix_mistakes.html#article-start
+
