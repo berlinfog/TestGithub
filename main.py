3607e36ee142b5a82346c837d9d0c5f86d82efc4
berlinfog <k1075060506@gmail.com>
2022-04-20 13:00:53 -0700

    sum a and b

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-#!usrbinpython3
-#coding=utf-8
-
-print (Life is short, you need Python !)
 #!/usr/bin/python3
 #coding=utf-8
 
-print ("Life is short, you need Python !")
-
 a = 10
 b = 20
+c = a + b
+print("The value of c is  ", c)
 
